717e6fd40f45fd7e86aaa8a65545a86dbe084719
parksh5295 <parksh5295@gmail.com>
2025-05-02 13:35:58 +0900

    .

--- data_use/data_path.py
+++ data_use/data_path.py
@@ -3,9 +3,9 @@
 
 def load_data_path(data_number):
     if data_number == 1:
-        return "C:\\Users\\LG\\Desktop\\Algorithm\\team_project\\data\\DL_FIRE_SV-C2_608350(250314-05)\\fire_nrt_SV-C2_608350.csv"
+        return "..\\data\\DL_FIRE_SV-C2_608350(250314-05)\\fire_nrt_SV-C2_608350.csv"
     elif data_number == 2:
-        return "C:\\Users\\LG\\Desktop\\Algorithm\\team_project\\data\\DL_FIRE_SV-C2_608316(230402-12)\\fire_archive_SV-C2_608316.csv"
+        return "..\\data\\DL_FIRE_SV-C2_608316(230402-12)\\fire_archive_SV-C2_608316.csv"
     else:
         raise ValueError("Invalid data number")
 
